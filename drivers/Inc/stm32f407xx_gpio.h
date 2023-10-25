086bd3b60d27ceee3de1f4854f9278d58139a27c
Zacck Osiemo <coderv63@gmail.com>
2023-10-25 06:17:35 +0200

    Start implementing GPIO drivers

--- drivers/Inc/stm32f407xx_gpio.h
+++ drivers/Inc/stm32f407xx_gpio.h
@@ -13,7 +13,7 @@
 typedef struct
 {
 	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinMode; /*Possible valus from @GPIO_PIN_MODES*/
 	uint8_t GPIO_PinSpeed;
 	uint8_t GPIO_PuPdControl;
 	uint8_t GPIO_PinOPType;
@@ -51,5 +51,64 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
 //IRQ config and ISR handling
 void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi);
 void GPIO_IRQHandling(uint8_t PinNumber);
+
+
+// GPIO MODE MACROS
+#define GPIO_MODE_IN 0
+#define GPIO_MODE_OUT 1
+#define GPIO_MODE_ALTFN 2
+#define GPIO_MODE_ANALOG 3
+#define GPIO_MODE_IT_FT  4
+#define GPIO_MODE_IT_RT  5
+#define GPIO_MODE_IT_RFT  6
+
+//Output mode macros
+#define GPIO_MODE_OUT_PP 0
+#define GPIO_MODE_OUT_OD 1
+
+//SPEED MACROS
+#define GPIO_SPEED_LOW 0
+#define GPIO_SPEED_MEDIUM 1
+#define GPIO_SPEED_FAST 2
+#define GPIO_SPEED_HIGH 3
+
+//PULL UP and PULL DOWN MACROS
+#define GPIO_NO_PUPD 0
+#define GPIO_PIN_PU 1
+#define GPIO_PIN_PD 2
+
+
+//pin Number macros
+#define GPIO_PIN_NO_0 0
+#define GPIO_PIN_NO_1 1
+#define GPIO_PIN_NO_2 2
+#define GPIO_PIN_NO_3 3
+#define GPIO_PIN_NO_4 4
+#define GPIO_PIN_NO_5 5
+#define GPIO_PIN_NO_6 6
+#define GPIO_PIN_NO_7 7
+#define GPIO_PIN_NO_8 8
+#define GPIO_PIN_NO_9 9
+#define GPIO_PIN_NO_10 10
+#define GPIO_PIN_NO_11 11
+#define GPIO_PIN_NO_12 12
+#define GPIO_PIN_NO_13 13
+#define GPIO_PIN_NO_14 14
+#define GPIO_PIN_NO_15 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif /* INC_STM32F407XX_GPIO_H_ */
