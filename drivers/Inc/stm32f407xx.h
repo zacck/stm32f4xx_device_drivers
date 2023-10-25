d53ab036531f627f203efbe334d73e9865048da5
Zacck Osiemo <coderv63@gmail.com>
2023-10-25 03:55:48 +0200

    Add RCC struct and build macros to enable GPIOs

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -39,8 +39,7 @@
 #define GPIOG_BASEADDR			(AHB1PERIPH_BASE + 0x1800)
 #define GPIOH_BASEADDR			(AHB1PERIPH_BASE + 0x1C00)
 #define GPIOI_BASEADDR			(AHB1PERIPH_BASE + 0x2000)
-#define GPIOJ_BASEADDR			(AHB1PERIPH_BASE + 0x2400)
-#define GPIOK_BASEADDR			(AHB1PERIPH_BASE + 0x2800)
+#define RCC_BASEADDR 			(AHB1PERIPH_BASE + 0x3800)
 
 
 // APB1 Peripheral addressess
@@ -68,7 +67,7 @@
 
 /********************* Peripheral Register Structures **************************/
 
-
+//GPIO
 typedef struct
 {
 	__vo uint32_t MODER;
@@ -82,6 +81,77 @@ typedef struct
 	__vo uint32_t AFR[2];
 
 }GPIO_RegDef_t;
+
+
+//RCC
+typedef struct
+{
+	__vo uint32_t CR;
+	__vo uint32_t PLLCFGR;
+	__vo uint32_t CFGR;
+	__vo uint32_t CIR;
+	__vo uint32_t AHB1RSTR;
+	__vo uint32_t AHB2RSTR;
+	__vo uint32_t AHB3RSTR;
+	uint32_t RESERVED0;
+	__vo uint32_t APB1RSTR;
+	__vo uint32_t APB2RSTR;
+	uint32_t RESERVED1[2];
+	__vo uint32_t AHB1ENR;
+	__vo uint32_t AHB2ENR;
+	uint32_t RESERVED2;
+	__vo uint32_t APB1ENR;
+	__vo uint32_t APB2ENR;
+	uint32_t RESERVED3[2];
+	__vo uint32_t AHB1LPENR;
+	__vo uint32_t AHB2LPENR;
+	__vo uint32_t AHB3LPENR;
+	__vo uint32_t RESERVED4;
+	__vo uint32_t APB1LPENR;
+	__vo uint32_t APB2LPENR;
+	uint32_t RESERVED5[2];
+	__vo uint32_t BDCR;
+	__vo uint32_t CSR;
+	uint32_t RESERVED6[2];
+	__vo uint32_t SSCGR;
+	__vo uint32_t PLLI2SCFGR;
+	__vo uint32_t PLLSAICFGR;
+	__vo uint32_t DCKCFGR;
+
+
+
+}RCC_RegDef_t;
+
+
+/**** Peripheral definitions  **/
+
+#define GPIOA ((GPIO_RegDef_t *)GPIOA_BASEADDR)
+#define GPIOB ((GPIO_RegDef_t *)GPIOB_BASEADDR)
+#define GPIOC ((GPIO_RegDef_t *)GPIOC_BASEADDR)
+#define GPIOD ((GPIO_RegDef_t *)GPIOD_BASEADDR)
+#define GPIOE ((GPIO_RegDef_t *)GPIOE_BASEADDR)
+#define GPIOF ((GPIO_RegDef_t *)GPIOF_BASEADDR)
+#define GPIOG ((GPIO_RegDef_t *)GPIOG_BASEADDR)
+#define GPIOH ((GPIO_RegDef_t *)GPIOH_BASEADDR)
+#define GPIOI ((GPIO_RegDef_t *)GPIOI_BASEADDR)
+#define RCC   ((RCC_RegDef_t *)RCC_BASEADDR)
+
+//Clock enable and disable macros
+#define GPIOA_PCLK_EN()		(RCC->AHB1ENR |= (1 << 0))
+#define GPIOB_PCLK_EN()		(RCC->AHB1ENR |= (1 << 1))
+#define GPIOC_PCLK_EN()		(RCC->AHB1ENR |= (1 << 2))
+#define GPIOD_PCLK_EN()		(RCC->AHB1ENR |= (1 << 3))
+#define GPIOE_PCLK_EN()		(RCC->AHB1ENR |= (1 << 4))
+#define GPIOF_PCLK_EN()		(RCC->AHB1ENR |= (1 << 5))
+#define GPIOG_PCLK_EN()		(RCC->AHB1ENR |= (1 << 6))
+#define GPIOH_PCLK_EN()		(RCC->AHB1ENR |= (1 << 7))
+#define GPIOI_PCLK_EN()		(RCC->AHB1ENR |= (1 << 8))
+
+
+
+
+
+
 
 
 
